ad9055a3016c577cae89c4ba1a4df8fd0898b8c0
blabla <blabla@blabla.com>
2016-06-23 13:15:31 +0200

    Signed-off-by: blabla <blabla@blabla.com>

--- tools/zip_generator.py
+++ tools/zip_generator.py
@@ -45,7 +45,7 @@ def main():
     if(addon_dir.startswith('.')):
       # skip hidden dirs
       continue
-    if(addon_dir == "repositor.arabictv"):
+    if(addon_dir == "repositor.blabla"):
       # skip download directory
       continue
     create_zip_file(addon_dir)
